2a69ffa03c35d3296a825e9ae4adda1ac640b3cf
raselwebdevv <rm5629164@gmail.com>
2025-01-01 12:45:54 +0300

    Initial commit

--- app/page.js
+++ app/page.js
@@ -13,7 +13,6 @@ export default function Home() {
 
   return (
     <>
-    
     <div className="px-0 lg:px-6">
       <div className="my-5">
         <div className="relative md:w-3/4 lg:w-1/2 xl:w-2/5 m-auto bg-white text-black dark:bg-black dark:text-white p-5 md:p-10 rounded-md shadow">
@@ -72,8 +71,6 @@ export default function Home() {
         </div>
       </div>
     </div>
-    
-    
     </>
   );
 }
